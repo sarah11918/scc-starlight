7929e7b4d17c58992f8f381197d128b2d333118a
sarahrainsberger <sarahrainsberger@gmail.com>
2025-05-18 16:39:20 +0000

    update responsive images and CSS grid

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,19 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://www.summersidechoir.ca',
+	experimental: {
+		responsiveImages: true,
+	  },
+	  image: {
+		experimentalLayout: 'constrained',
+	  },
 	integrations: [
 		starlight({
 			title: 'Summerside Choir',
 			favicon: '/scclogo.png',
+			logo: {
+				src: './src/assets/SSCC_LOGO.jpg',
+			},
 			social: [
 				{ icon: 'facebook', label: 'Facebook', href: 'https://www.facebook.com/pages/Summerside-Community-Choir/125415017614194' },
 				{ icon: 'youtube', label: 'YouTube', href: 'https://www.youtube.com/user/SummersideChoirPEI/' },
