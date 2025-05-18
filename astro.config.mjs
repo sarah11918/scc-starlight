209b177b7673fbf20be0b8294a670541310e473b
sarahrainsberger <sarahrainsberger@gmail.com>
2025-05-18 16:19:47 +0000

    add site info in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://www.summersidechoir.ca',
 	integrations: [
 		starlight({
 			title: 'Summerside Choir',
